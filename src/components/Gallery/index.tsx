203ce109e8fa86ebc9c7f5f597ca6118e6299d47
jhonathandsg10 <jhonathan.gomes@estudane.ifms.edu.br>
2023-03-09 20:48:10 -0400

    aula 09/03- adicionado mais coisas

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,25 +1,42 @@
-import {movies} from "../Gallery/data"
+import { movies } from "../Gallery/data"
 import { useState } from "react"
-export function Gallery(){
+import { Title } from "../Title/Index";
+import '../Gallery/style.css'
+
+export function Gallery() {
     const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
 
     function handleClickNext() {
-        setIndex(index + 1);
+        if (index < movies.length - 1)
+            setIndex(index + 1);
     }
     function handleClickPrev() {
-        setIndex(index - 1);
+        if (index > 0)
+            setIndex(index - 1);
     }
     let movie = movies[index]
 
-    return(
+    return (
         <>
+            <h2 className="titulo">
+                <i>{movie.title} </i>
+            </h2>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Esconder' : 'Mostrar'} sinopse
+            </button>
+            {showMore && <p className="sinopse">{movie.synopsis}</p>}
             <section className="gallery">
                 <img src={movie.images.poster} />
             </section>
 
             <button onClick={handleClickPrev}>Anterior</button>
             <button onClick={handleClickNext}>Próximo</button>
-            
+
         </>
     )
 }
