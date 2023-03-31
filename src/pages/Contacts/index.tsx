a13a0abb4e893c2a3236383e7a03c567f6519be9
jhonathandsg10 <jhonathan.gomes@estudante.ifms.edu.br>
2023-03-30 21:03:21 -0400

    att aula 30/03

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,4 +1,4 @@
-import'./style.css'
+import './style.css'
 import { useState, useEffect } from "react";
 import { Contact } from "../../Types";
 import { ContactCard } from "../../components/ContactsCard";
@@ -6,26 +6,32 @@ import { Title } from "../../components/Title/Index";
 import { Input } from "../../components/Input";
 import { getContacts } from "../../services/api";
 import { Contacts } from "../../components/Contacts";
+import { BaseLayout } from '../../layout/BaseLayout';
+import { CircularProgress, TextField } from '@mui/material';
 
 export function Contacts1() {
     const [search, setSearch] = useState('');
+    const [isLoading, setIsLoading] = useState<boolean>(false)
     const [contacts, setContacts] = useState<Contact[]>([]);
+
+    const filteredContacts = ()=>
 
     useEffect(() => {
         async function listContacts() {
-            setContacts(await getContacts())
+            setIsLoading(true)
+            setContacts(await getContacts());
+            setIsLoading(false)
         }
         listContacts();
         console.log(contacts);
     }, [])
 
     return (
-        <>
-            <header>
-                <Title text="Agenda de Contatos" />
-            </header>
-            <main>
-                <Input placeholder='Localizar' type='text' />
+
+        <BaseLayout appBarTitle='Agenda de Contatos'>
+            <TextField variant='outlined' fullWidth />
+
+            {isLoading ? (<CircularProgress />) : (
                 <Contacts >
                     {
                         contacts.map(contact => {
@@ -33,9 +39,14 @@ export function Contacts1() {
                         })
                     }
                 </Contacts>
+            )}
 
-            </main>
 
-        </>
+
+
+
+        </BaseLayout>
+
+
     )
 }
